022ca3a05b77bae6f1b18c3adef8d374f8c7d8e6
sondresr <sondre.strand.ravn@gmail.com>
2014-02-06 16:11:28 +0100

    Create network.go

---
+++ network.go
@@ -0,0 +1,34 @@
+package main
+
+import(
+    "net"
+    "fmt"
+)
+
+func connect()(*net.UDPConn, *net.UDPConn){
+    localport       := ":20016"
+    inAddr,_          := net.ResolveUDPAddr("udp4",localport)
+    out_socket,err := net.DialUDP("udp4",&net.UDPAddr{IP: net.IPv4(129,241,187,147),
+    Port: 20016,},&net.UDPAddr{IP: net.IPv4(129,241,187,255), Port: 20016,})
+    CheckError(err)
+    in_socket, err  := net.ListenUDP("udp4",inAddr)
+    CheckError(err)
+    return out_socket, in_socket
+}
+
+func CheckError(err error){
+    if err!=nil{
+        fmt.Println(err.Error())
+    }    
+}
+
+func write(out_socket *net.UDPConn, message string){
+    out_socket.Write([]byte(message+"\x00"))
+}
+
+func read(in_socket *net.UDPConn)(string){
+   var buffer [1024]byte
+   n, ip,err    := in_socket.ReadFromUDP(buffer[0:]) 
+   message      := string(buffer[0:n])
+   return message 
+}
